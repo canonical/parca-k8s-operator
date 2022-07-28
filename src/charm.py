e814f1fa00a7207393d5632aba62e8beac6384bc
Jon Seager <jon@sgrs.uk>
2022-07-28 22:50:20 +0100

    Initial commit/transplant from machine operator

---
+++ src/charm.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# Copyright 2022 Jon Seager
+# See LICENSE file for licensing details.
+
+"""Charmed Operator to deploy Parca - a continuous profiling tool."""
+
+import logging
+
+from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from charms.parca.v0.parca_config import (
+    DEFAULT_CONFIG_PATH,
+    ParcaConfig,
+    parca_command_line,
+)
+from charms.prometheus_k8s.v0.prometheus_scrape import (
+    MetricsEndpointConsumer,
+    MetricsEndpointProvider,
+)
+from lightkube.models.core_v1 import ServicePort
+from ops import pebble
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import ActiveStatus, Container, MaintenanceStatus, WaitingStatus
+from ops.pebble import Layer
+
+logger = logging.getLogger(__name__)
+
+
+class ParcaOperatorCharm(CharmBase):
+    """Charmed Operator to deploy Parca - a continuous profiling tool."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        # Observe common Juju events
+        self.framework.observe(self.on.parca_pebble_ready, self._parca_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._config_changed)
+        self.framework.observe(self.on.update_status, self._update_status)
+
+        # Patch the Kubernetes service to contain the correct port
+        port = ServicePort(7070, name=f"{self.app.name}")
+        self.service_patcher = KubernetesServicePatch(self, [port])
+
+        # The profiling_consumer handles the relation that allows Parca to scrape other apps in the
+        # model that provide a "profiling-endpoint" relation
+        self.profiling_consumer = MetricsEndpointConsumer(self, relation_name="profiling-endpoint")
+        self.framework.observe(
+            self.profiling_consumer.on.targets_changed, self._on_profiling_targets_changed
+        )
+
+        # The metrics_endpoint_provider enables Parca to be scraped by Prometheus for metrics
+        self.metrics_endpoint_provider = MetricsEndpointProvider(
+            self,
+            jobs=[{"static_configs": [{"targets": ["*:7070"]}]}],
+            relation_name="metrics-endpoint",
+        )
+
+        self.container: Container = self.unit.get_container("parca")
+
+    def _parca_pebble_ready(self, event):
+        """Define and start a workload using the Pebble API."""
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Configure Parca by writing the config file to the container
+        scrape_config = self.profiling_consumer.jobs()
+        self._configure(self.config, scrape_config, restart=False)
+        
+        # Define an initial Pebble layer
+        container.add_layer("parca", self._pebble_layer, combine=True)
+        container.replan()
+        self.unit.status = ActiveStatus()
+
+    def _update_status(self, _):
+        """Performed on an interval dictated by model config."""
+        self.unit.set_workload_version(self.version)
+
+    def _config_changed(self, _):
+        """Update the configuration files, restart parca."""
+        self.unit.status = MaintenanceStatus("reconfiguring parca")
+        scrape_config = self.profiling_consumer.jobs()
+        
+        # Try to configure Parca
+        if self.container.can_connect():
+            self._configure(self.config, scrape_config)
+            self.unit.status = ActiveStatus()
+        else:
+            self.unit.status = WaitingStatus("waiting for container")
+
+    def _on_profiling_targets_changed(self, _):
+        """Update the Parca scrape configuration according to present relations."""
+        self.unit.status = MaintenanceStatus("reconfiguring parca")
+        self._configure(self.config, self.profiling_consumer.jobs())
+        self.unit.status = ActiveStatus()
+    
+    def _configure(self, scrape_configs=[], *, restart=True):
+        """Configure Parca in the container. Restart Parca by default."""
+        # Write the config file
+        parca_config = ParcaConfig(scrape_configs)
+        if self.container.can_connect():
+            # TODO(jnsgruk): add user/group details when container is updated
+            self.container.push(DEFAULT_CONFIG_PATH, str(parca_config), make_dirs=True, permissions=0o644)
+            if restart:
+                self.container.restart("parca")
+
+    @property
+    def version(self) -> str:
+        """Reports the version of Parca."""
+        if self.container.can_connect():
+            process = self.container.exec(['parca', '--version'], encoding="utf-8")
+            try:
+                stdout, _ = process.wait_output()
+            except pebble.ExecError as e:
+                raise e
+            
+            splits = stdout.read().split(" ")
+            # If we're not on a 'proper' released version, include the first few digits of
+            # the commit we're build from
+            if "-next" in splits[2]:
+                return f"{splits[2]}+{splits[4][:6]}"
+            return splits[2]
+
+    @property
+    def _pebble_layer(self) -> Layer:
+        """Returns a Pebble layer for Parca based on the current configuration."""
+        return Layer({
+            "services": {
+                "parca": {
+                    "override": "replace",
+                    "summary": "parca",
+                    "command": parca_command_line(self.config),
+                    "startup": "enabled",
+                }
+            },
+        })
+
+
+
+if __name__ == "__main__":  # pragma: nocover
+    main(ParcaOperatorCharm)
---
+++ tests/unit/test_charm.py
@@ -0,0 +1,171 @@
+# Copyright 2022 Jon Seager
+# See LICENSE file for licensing details.
+
+# This file contains basic tests simply to ensure that the various event handlers for operator
+# framework are being called, and that they in turn are invoking the right helpers.
+#
+# The helpers themselves require too much mocking, and are validated in functional/integration
+# tests.
+
+
+import json
+import unittest
+from io import StringIO
+from subprocess import CalledProcessError
+from unittest.mock import PropertyMock, patch
+
+import ops.testing
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus
+from ops.pebble import ExecError
+from ops.testing import Harness
+
+from charm import ParcaOperatorCharm
+
+ops.testing.SIMULATE_CAN_CONNECT = True
+
+SCRAPE_METADATA = {
+    "model": "test-model",
+    "model_uuid": "abcdef",
+    "application": "profiled-app",
+    "charm_name": "test-charm",
+}
+SCRAPE_JOBS = [
+    {
+        "global": {"scrape_interval": "1h"},
+        "rule_files": ["/some/file"],
+        "file_sd_configs": [{"files": "*some-files*"}],
+        "job_name": "my-first-job",
+        "metrics_path": "/one-path",
+        "static_configs": [{"targets": ["*:7000"], "labels": {"some-key": "some-value"}}],
+    },
+]
+
+class MockExec:
+    def __init__(self, stdout, stderr=""):
+        self.stdout = stdout
+        self.stderr = stderr
+    
+    def wait_output(self):
+        return StringIO(self.stdout), StringIO(self.stderr)
+
+
+class TestCharm(unittest.TestCase):
+    @patch("charm.KubernetesServicePatch", lambda x,y: True)
+    def setUp(self):
+        self.harness = Harness(ParcaOperatorCharm)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    @patch("parca.Container.exec")
+    def test_parca_version_next(self, exec):
+        self.harness.set_can_connect("parca", True)
+        exec.return_value = MockExec("parca, version v0.12.0-next (commit: deadbeef)\n")
+        self.assertEqual(self.harness.charm.version, "v0.12.0-next+deadbe")
+
+    @patch("parca.Container.exec")
+    def test_parca_version_tagged(self, exec):
+        self.harness.set_can_connect("parca", True)
+        exec.return_value = MockExec("parca, version v0.13.0 (commit: deadbeef")
+        self.assertEqual(self.harness.charm.version, "v0.13.0")
+
+    @patch("parca.Container.exec")
+    def test_parca_version_tagged(self, exec):
+        exec.side_effect = ExecError("foobar", 1, "", "")
+        try:
+            self.harness.charm.version
+        except ExecError as e:
+            self.assertEqual("non-zero exit code 1 executing 'foobar', stdout='', stderr=''", str(e))
+
+   
+    # @patch("charm.snap.hold_refresh")
+    # @patch("parca.Parca.version", new_callable=PropertyMock(return_value="v0.12.0"))
+    # def test_update_status(self, _, hold):
+    #     self.harness.charm.on.update_status.emit()
+    #     hold.assert_called_once()
+    #     self.assertEqual(self.harness.get_workload_version(), "v0.12.0")
+
+    # @patch("charm.Parca.configure")
+    # def test_config_changed(self, configure):
+    #     config = {
+    #         "storage-persist": False,
+    #         "memory-storage-limit": 1024,
+    #     }
+    #     self.harness.update_config(config)
+    #     configure.assert_called_with(config, [])
+    #     self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
+
+    # @patch("charm.Parca.configure")
+    # def test_profiling_endpoint_relation(self, _):
+    #     # Create a relation to an app named "profiled-app"
+    #     rel_id = self.harness.add_relation("profiling-endpoint", "profiled-app")
+    #     # Simulate that "profiled-app" has provided the data we're expecting
+    #     self.harness.update_relation_data(
+    #         rel_id,
+    #         "profiled-app",
+    #         {
+    #             "scrape_metadata": json.dumps(SCRAPE_METADATA),
+    #             "scrape_jobs": json.dumps(SCRAPE_JOBS),
+    #         },
+    #     )
+    #     # Add a unit to the relation
+    #     self.harness.add_relation_unit(rel_id, "profiled-app/0")
+    #     # Simulate the remote unit adding its details for scraping
+    #     self.harness.update_relation_data(
+    #         rel_id,
+    #         "profiled-app/0",
+    #         {
+    #             "prometheus_scrape_unit_address": "1.1.1.1",
+    #             "prometheus_scrape_unit_name": "profiled-app/0",
+    #         },
+    #     )
+    #     # Taking into account the data provided by the simulated app, we should receive the
+    #     # following jobs config from the profiling_consumer
+    #     expected = [
+    #         {
+    #             "metrics_path": "/one-path",
+    #             "static_configs": [
+    #                 {
+    #                     "labels": {
+    #                         "some-key": "some-value",
+    #                         "juju_model": "test-model",
+    #                         "juju_model_uuid": "abcdef",
+    #                         "juju_application": "profiled-app",
+    #                         "juju_charm": "test-charm",
+    #                         "juju_unit": "profiled-app/0",
+    #                     },
+    #                     "targets": ["1.1.1.1:7000"],
+    #                 }
+    #             ],
+    #             "job_name": "juju_test-model_abcdef_profiled-app_test-charm_prometheus_scrape_my-first-job",
+    #             "relabel_configs": [
+    #                 {
+    #                     "source_labels": [
+    #                         "juju_model",
+    #                         "juju_model_uuid",
+    #                         "juju_application",
+    #                         "juju_unit",
+    #                     ],
+    #                     "separator": "_",
+    #                     "target_label": "instance",
+    #                     "regex": "(.*)",
+    #                 }
+    #             ],
+    #         }
+    #     ]
+    #     self.assertEqual(self.harness.charm.profiling_consumer.jobs(), expected)
+
+    # @patch("charm.Parca.configure")
+    # @patch("socket.getfqdn", new=lambda *args: "some.host")
+    # def test_metrics_endpoint_relation(self, _):
+    #     # Create a relation to an app named "prometheus"
+    #     rel_id = self.harness.add_relation("metrics-endpoint", "prometheus")
+    #     # Add a prometheus unit
+    #     self.harness.add_relation_unit(rel_id, "prometheus/0")
+    #     # Grab the unit data from the relation
+    #     unit_data = self.harness.get_relation_data(rel_id, self.harness.charm.unit.name)
+    #     # Ensure that the unit set its targets correctly
+    #     expected = {
+    #         "prometheus_scrape_unit_address": "some.host",
+    #         "prometheus_scrape_unit_name": "parca/0",
+    #     }
+    #     self.assertEqual(unit_data, expected)
