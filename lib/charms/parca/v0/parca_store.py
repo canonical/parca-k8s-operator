c35095ed5a74edf10b556c16617bffc96ec628eb
Jon Seager <jon@sgrs.uk>
2023-08-25 16:06:59 +0100

    chore: bump the parca_store lib 0.2 -> 0.3

--- lib/charms/parca/v0/parca_store.py
+++ lib/charms/parca/v0/parca_store.py
@@ -1,7 +1,35 @@
-"""## Overview.
+"""# Overview.
 
 This document explains how to integrate with the Parca charm where you wish to use Parca
 as a store for profiles that are sent from a Parca Agent.
+
+## Requirer Library usage
+
+In this mode, your charm will be an application that needs to receive store configuration over the
+relation in order that the workload can be configured to use a remote store for profiles.
+
+To begin, start by importing the library and subscribing to some events:
+
+```python
+from charms.parca.v0.parca_store import (ParcaStoreEndpointRequirer, RemoveStoreEvent)
+
+def __init__(self, *args):
+    super().__init__(*args)
+    # ...
+    self.store_requirer = ParcaStoreEndpointRequirer(
+        self, relation_name="external-parca-store-endpoint"
+    )
+    self.framework.observe(self.store_requirer.on.endpoints_changed, self._configure_store)
+    self.framework.observe(self.store_requirer.on.remove_store, self._configure_store)
+    # ...
+
+def _configure_store(self, event):
+    store_config = {} if isinstance(event, RemoveStoreEvent) else event.store_config
+    self.application.configure(store_config=store_config)
+    # ...
+```
+
+You can also fetch the store config at any time by using `self.store_requirer.config`.
 
 ## Provider library usage
 
@@ -63,6 +91,7 @@ Where `self._bearer_token_generator` can be any `Callable` that returns a string
 """
 
 import ipaddress
+import json
 import socket
 from typing import Callable
 from urllib.parse import urlparse
@@ -77,7 +106,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 3
 
 
 DEFAULT_RELATION_NAME = "parca-store-endpoint"
@@ -164,3 +193,113 @@ class ParcaStoreEndpointProvider(ops.Object):
             return True
         except ValueError:
             return False
+
+
+class StoreEndpointsChangedEvent(ops.EventBase):
+    """Event emitted when Parca store endpoints change."""
+
+    def __init__(
+        self,
+        handle,
+        relation_id,
+        remote_store_address,
+        remote_store_bearer_token,
+        remote_store_insecure,
+    ):
+        super().__init__(handle)
+        self.relation_id = relation_id
+        self.store_config = {
+            "remote-store-address": remote_store_address,
+            "remote-store-bearer-token": remote_store_bearer_token,
+            "remote-store-insecure": remote_store_insecure,
+        }
+
+    def snapshot(self):
+        """Save store relation information."""
+        return {"relation_id": self.relation_id, "store_config": json.dumps(self.store_config)}
+
+    def restore(self, snapshot):
+        """Restore store relation information."""
+        self.relation_id = snapshot["relation_id"]
+        self.store_config = json.loads(snapshot["store_config"])
+
+
+class RemoveStoreEvent(ops.EventBase):
+    """Event emitted when Parca store config should be removed."""
+
+    def __init__(self, handle, relation_id):
+        super().__init__(handle)
+        self.relation_id = relation_id
+
+    def snapshot(self):
+        """Save store relation information."""
+        return {"relation_id": self.relation_id}
+
+    def restore(self, snapshot):
+        """Restore store relation information."""
+        self.relation_id = snapshot["relation_id"]
+
+
+class ParcaStoreEvents(ops.ObjectEvents):
+    """Event descriptor for events raised by `ParcaStoreEndpointRequirer`."""
+
+    endpoints_changed = ops.EventSource(StoreEndpointsChangedEvent)
+    remove_store = ops.EventSource(RemoveStoreEvent)
+
+
+class ParcaStoreEndpointRequirer(ops.Object):
+    """Provide an interface for apps that need to send data to a Parca Store."""
+
+    on = ParcaStoreEvents()
+
+    def __init__(self, charm, relation_name: str = DEFAULT_RELATION_NAME):
+        """Construct a Parca profile store requirer.
+
+        Args:
+            charm: a `ops.CharmBase` object that manages this
+                `ParcaStoreEndpointRequirer` object. Typically this is `self` in the instantiating
+                class.
+            relation_name: an optional string that denotes the name of the relation endpoint.
+        """
+        super().__init__(charm, relation_name)
+        self._charm = charm
+        self._relation_name = relation_name
+
+        events = self._charm.on[self._relation_name]
+        self.framework.observe(events.relation_changed, self._on_parca_store_relation_changed)
+        self.framework.observe(events.relation_departed, self._on_parca_store_relation_departed)
+
+    def _on_parca_store_relation_changed(self, event):
+        """Handle changes with related store requirers.
+
+        Args:
+            event: a `CharmEvent` in response to which the Parca store client
+                must update its store configuration.
+        """
+        rel_id = event.relation.id
+        rel_data = event.relation.data[event.relation.app]
+        self.on.endpoints_changed.emit(
+            relation_id=rel_id,
+            remote_store_address=rel_data["remote-store-address"],
+            remote_store_bearer_token=rel_data["remote-store-bearer-token"],
+            remote_store_insecure=rel_data["remote-store-insecure"],
+        )
+
+    def _on_parca_store_relation_departed(self, event):
+        """Notify requirers that the store config they received should be removed.
+
+        Args:
+            event: a `CharmEvent` in response to which the Parca store client
+                must disregard its store configuration
+        """
+        rel_id = event.relation.id
+        self.on.remove_store.emit(relation_id=rel_id)
+
+    @property
+    def config(self) -> dict:
+        """Return the store config for a given requirer if the relation is formed."""
+        if relation := self._charm.model.get_relation(self._relation_name):
+            keys = ["remote-store-address", "remote-store-bearer-token", "remote-store-insecure"]
+            return {k: relation.data[relation.app].get(k, "") for k in keys}
+        else:
+            return {}
