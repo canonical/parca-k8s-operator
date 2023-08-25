e25acd5214dacc596dd751786476b7e8ed2f16bb
Jon Seager <jon@sgrs.uk>
2023-08-25 11:57:26 +0100

    test: simplify version fetching tests, improve coverage

--- tests/unit/test_parca.py
+++ tests/unit/test_parca.py
@@ -6,8 +6,10 @@ from unittest.mock import MagicMock, patch
 
 from parca import Parca
 
-MOCK_WEB_RESPONSE_V1 = b'<!doctype html><html lang="en"><head><script>window.PATH_PREFIX="",window.APP_VERSION="v0.18.0-2b08f0bd"</script><meta charset="utf-8"/><link rel="icon" href="/favicon.svg"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Open Source Infrastructure-wide continuous profiling"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/><title>Parca</title><script defer="defer" src="/static/js/main.e6a20ad1.js"></script><link href="/static/css/main.943f71c0.css" rel="stylesheet"></head><body class="bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-200"><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>'
-MOCK_WEB_RESPONSE_V2 = b'<!doctype html><html lang="en"><head><script>window.PATH_PREFIX="",window.APP_VERSION="v0.18.0"</script><meta charset="utf-8"/><link rel="icon" href="/favicon.svg"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Open Source Infrastructure-wide continuous profiling"/><link rel="apple-touch-icon" href="/logo192.png"/><link rel="manifest" href="/manifest.json"/><title>Parca</title><script defer="defer" src="/static/js/main.e6a20ad1.js"></script><link href="/static/css/main.943f71c0.css" rel="stylesheet"></head><body class="bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-200"><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>'
+# Extract from a real response that Parca issued to test the regular expression works for capturing
+# the version from the served page.
+MOCK_WEB_RESPONSE_V1 = b'<script>window.PATH_PREFIX="",window.APP_VERSION="v0.18.0-2b08f0bd"</s'
+MOCK_WEB_RESPONSE_V2 = b'<script>window.PATH_PREFIX="",window.APP_VERSION="v0.18.0"</s>'
 
 
 class TestParca(unittest.TestCase):
@@ -48,7 +50,17 @@ class TestParca(unittest.TestCase):
 
         self.assertEqual(self.parca._fetch_version(), "0.18.0")
 
-    @patch("parca.Parca._fetch_version")
+    @patch("urllib.request.urlopen")
     def test_version_fetch_raises_empty_string_response(self, fv):
         fv.side_effect = Exception
+        # Don't wait 3 seconds in between retry calls
+        self.parca._version_retry_wait = 0.1
         self.assertEqual(self.parca.version, "")
+
+    @patch("urllib.request.urlopen")
+    def test_fetch_version_retries(self, uo):
+        uo.side_effect = Exception
+        # Don't wait 3 seconds in between retry calls
+        self.parca._version_retry_wait = 0.1
+        self.parca._fetch_version()
+        self.assertEqual(uo.call_count, 3)
