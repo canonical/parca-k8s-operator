86541f42508c68538257963e0a5f205543d12ffa
Jon Seager <jon@sgrs.uk>
2024-08-27 14:37:42 +0100

    test: await parca_charm fixture

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -16,9 +16,11 @@ PARCA = "parca"
 
 @mark.abort_on_fail
 async def test_deploy(ops_test: OpsTest, parca_charm, parca_oci_image):
+    charm = await parca_charm
+
     await asyncio.gather(
         ops_test.model.deploy(
-            parca_charm,
+            charm,
             resources={"parca-image": parca_oci_image},
             application_name=PARCA,
         ),
--- tests/integration/test_ingress_traefik.py
+++ tests/integration/test_ingress_traefik.py
@@ -17,10 +17,11 @@ PARCA = "parca-k8s"
 async def test_ingress_traefik_k8s(ops_test, parca_charm, parca_oci_image):
     """Test that Parca can be related with Traefik for ingress."""
     apps = [PARCA, TRAEFIK]
+    charm = await parca_charm
 
     await asyncio.gather(
         ops_test.model.deploy(
-            parca_charm,
+            charm,
             resources={"parca-image": parca_oci_image},
             application_name=PARCA,
         ),
