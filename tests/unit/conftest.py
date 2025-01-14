f325af78655fb18eb742674d287f0f369b2b95f9
Pietro Pasotti <starfire.daemon@gmail.com>
2025-01-14 14:08:51 +0100

    fmt

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,5 +5,8 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="session")
 def diable_charm_tracing_buffer():
-    with patch("charms.tempo_coordinator_k8s.v0.charm_tracing._BufferedExporter.export", lambda _, __: None):
+    with patch(
+        "charms.tempo_coordinator_k8s.v0.charm_tracing._BufferedExporter.export",
+        lambda _, __: None,
+    ):
         yield
