a064c9d4186e1703cee8a7738872561f2e5157d8
Pietro Pasotti <starfire.daemon@gmail.com>
2025-01-15 12:01:17 +0100

    icon

--- src/charm.py
+++ src/charm.py
@@ -79,7 +79,7 @@ class ParcaOperatorCharm(ops.CharmBase):
             self,
             item=CatalogueItem(
                 "Parca UI",
-                icon="bar-chart",
+                icon="apm-trace",
                 url=self.external_url,
                 description="""Continuous profiling backend. Allows you to collect, store,
                  query and visualize profiles from your distributed deployment.""",
