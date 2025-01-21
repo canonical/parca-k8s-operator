091caa37fa1b1fb18984748a3cbcb028145d2dac
michael <michael.dmitry98@gmail.com>
2025-01-21 13:57:55 +0200

    fix tests

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
+
 import asyncio
+from subprocess import getstatusoutput
 
 import pytest
-import requests
-from helpers import get_unit_ip
+from helpers import get_unit_fqdn
 
 from nginx import NGINX_PORT
 
 PARCA = "parca"
 SSC = "self-signed-certificates"
+# Path where SSC saves the CA certificate
+SSC_CA_CERT_PATH = "/tmp/ca-cert.pem"
 
 
 @pytest.mark.abort_on_fail
@@ -42,6 +45,12 @@ async def test_setup(ops_test, parca_charm, parca_resources):
 
 @pytest.mark.abort_on_fail
 async def test_direct_url_200(ops_test, ca_cert):
-    parca_ip = get_unit_ip(ops_test.model_name, PARCA, 0)
-    url = f"https://{parca_ip}:{NGINX_PORT}"
-    assert requests.get(url, verify=ca_cert).status_code == 200
+    parca_address = get_unit_fqdn(ops_test.model_name, PARCA, 0)
+    url = f"https://{parca_address}:{NGINX_PORT}"
+    # Parca's certificate only contains the fqdn address of parca as SANs.
+    # To query the parca server with TLS while validating the certificate, we need to perform the query
+    # against the parca server's fqdn.
+    # We can do that from inside another K8s pod, such as ssc.
+    cmd = f"""juju exec --model {ops_test.model_name} --unit {SSC}/0 "curl --cacert {SSC_CA_CERT_PATH} {url}" """
+    exit_code, output = getstatusoutput(cmd)
+    assert exit_code == 0, f"Failed to query {url}. {output}"
