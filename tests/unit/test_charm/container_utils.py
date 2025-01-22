ede7246951336b8141d66e4c4045a3735558d137
Pietro Pasotti <starfire.daemon@gmail.com>
2025-01-22 15:54:41 +0100

    docstrings for utils functions

--- tests/unit/test_charm/container_utils.py
+++ tests/unit/test_charm/container_utils.py
@@ -7,11 +7,13 @@ from parca import DEFAULT_CONFIG_PATH
 
 
 def assert_parca_command_equals(state: State, expected_command):
+    """Assert that the command line for the parca service in the parca container matches."""
     container = state.get_container("parca")
     assert container.plan.services["parca"].command == expected_command
 
 
 def assert_parca_config_equals(context: Context, state: State, expected_config):
+    """Assert that the parca config file in the container matches."""
     container = state.get_container("parca")
 
     config = container.get_filesystem(context).joinpath(Path(DEFAULT_CONFIG_PATH).relative_to("/"))
